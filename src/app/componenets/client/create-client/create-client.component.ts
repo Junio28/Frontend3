0dcb223376045ea5ae8892b8fd1c1a3a13ab523a
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-27 17:00:40 -0500

    Proceso del Crear Client

--- src/app/componenets/client/create-client/create-client.component.ts
+++ src/app/componenets/client/create-client/create-client.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { ClientsService } from '../../../services/clients.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-client',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateClientComponent implements OnInit {
 
-  constructor() { }
+  // @HostBinding('class') classes = 'row';
+
+  client: any = {
+    id: 0,
+    name: '',
+    lastname: '',
+    email: '',
+    rut: '',
+    address: '',
+    phone: ''
+  }
+  constructor(private clientsService: ClientsService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    //const params = this.activatedRoute.snapshot.params;
   }
 
+  saveNewClient(){
+    this.clientsService.addClient(this.client)
+      .subscribe(
+        res => {
+          this.router.navigate(['/cliente']);
+        },
+        err => console.error(err)
+      )
+  }
 }
--- src/app/services/clients.service.ts
+++ src/app/services/clients.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ClientI } from '../models/client-i';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsService {
   API_URI = 'http://localhost:4000';
+  client: ClientI
 
   constructor(private http: HttpClient) { }
 
   getClients(){
     return this.http.get(`${this.API_URI}/clients`);
+  }
+
+  addClient(user: ClientI){
+    return this.http.post(`${this.API_URI}/clients`, user);
   }
 
   deleteClient(id: string){
