17c102063067b9450f68105f639303c899c4977a
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-27 19:25:10 -0500

    Proceso del Crear TypeProduct

--- src/app/componenets/type-product/create-type-product/create-type-product.component.ts
+++ src/app/componenets/type-product/create-type-product/create-type-product.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { TypeProductsService } from '../../../services/type-products.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-type-product',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTypeProductComponent implements OnInit {
 
-  constructor() { }
+  // @HostBinding('class') classes = 'row';
+  typeproduct: any = {
+    id: 0,
+    name: ''
+  }
+  constructor(private typeproductsService: TypeProductsService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    //const params = this.activatedRoute.snapshot.params;
   }
 
+  saveNewTypeProduct(){
+    this.typeproductsService.addTypeProduct(this.typeproduct)
+      .subscribe(
+        res => {
+          this.router.navigate(['/tipo_producto']);
+        },
+        err => console.error(err)
+      )
+  }
 }
--- src/app/services/type-products.service.ts
+++ src/app/services/type-products.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TypeProductI } from '../models/type-product-i';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TypeProductsService {
   API_URI = 'http://localhost:4000';
+  typeproduct: TypeProductI
 
   constructor(private http: HttpClient) { }
 
   getTypeProducts(){
     return this.http.get(`${this.API_URI}/type_products`);
+  }
+
+  addTypeProduct(typeproduct: TypeProductI){
+    return this.http.post(`${this.API_URI}/type_products`, typeproduct);
   }
 
   deleteTypeProduct(id: string){
